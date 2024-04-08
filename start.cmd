e43f8653147dc3b7442e2512d1abfdcc0dfdc4a5
Nickolaj <nikolajpetrishchev@gmail.com>
2024-04-08 18:48:36 +0500

    add start-file

---
+++ start.cmd
@@ -0,0 +1,6 @@
+@echo off
+start cmd /k "call env\scripts\activate && python manage.py migrate"
+start cmd /k "call env\scripts\activate && python manage.py runserver"
+start cmd /k "call env\scripts\activate && python manage.py runbot"
+start cmd /k "call env\scripts\activate && celery -A django_conf worker -l info -P threads"
+start cmd /k "call env\scripts\activate && celery -A django_conf beat -l info"
